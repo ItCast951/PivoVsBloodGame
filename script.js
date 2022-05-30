13a0e49d55935f51ec0302d7d0b21ac740233bc5
ItCast <61616765+ItCast951@users.noreply.github.com>
2022-05-30 20:34:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,56 @@ var newFactory;
 var interval = 800;
 var counter = 1;
 var countStr = "Кружка номер ";
+const TOKEN = process.env.TELEGRAM_TOKEN || '5595401217:AAHoX02Is2qQzmobZapvkLRNPOVRAs_Htp4';
+const gameName = process.env.TELEGRAM_GAMENAME || 'Pivo';
+// Specify '0' to use ngrok i.e. localhost tunneling
+let url = process.env.URL || 'https://<PUBLIC-URL>';
+const port = process.env.PORT || 8080;
+
+const TelegramBot = require('../..');
+const express = require('express');
+const path = require('path');
+
+const bot = new TelegramBot(TOKEN, { polling: true });
+const app = express();
+
+// Basic configurations
+app.set('view engine', 'ejs');
+
+// Tunnel to localhost.
+// This is just for demo purposes.
+// In your application, you will be using a static URL, probably that
+// you paid for. :)
+if (url === '0') {
+    const ngrok = require('ngrok');
+    ngrok.connect(port, function onConnect(error, u) {
+        if (error) throw error;
+        url = u;
+        console.log(`Game tunneled at ${url}`);
+    });
+}
+
+// Matches /start
+bot.onText(/\/start/, function onPhotoText(msg) {
+    bot.sendGame(msg.chat.id, gameName);
+});
+
+// Handle callback queries
+bot.on('callback_query', function onCallbackQuery(callbackQuery) {
+    bot.answerCallbackQuery(callbackQuery.id, { url });
+});
+
+// Render the HTML game
+app.get('/', function requestListener(req, res) {
+    res.sendFile(path.join(__dirname, 'game.html'));
+});
+
+// Bind server to port
+app.listen(port, function listen() {
+    console.log(`Server is listening at http://localhost:${port}`);
+});
+
+
 
 function createGame() {
     for (let i = 0; i < 30; i++) {
